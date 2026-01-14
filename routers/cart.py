01a5e7fa916304feebf514d32078a9e399c320e3
trulsasb <trulsasb@gmail.com>
2026-01-14 15:25:03 +0100

    Update cart.py
    Ny linje 4

--- routers/cart.py
+++ routers/cart.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from typing import List
-from .. import models, database
+from models import models, database
+
 from pydantic import BaseModel
 
 router = APIRouter()
