70b0c7c89560172dcfea04f4471195d48199c0ad
trulsasb <trulsasb@gmail.com>
2026-01-15 11:31:07 +0100

    Update database.py
    rester databasesti

--- database.py
+++ database.py
@@ -2,7 +2,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
+print("DATABASE_URL_IN_USE:", DATABASE_URL)
+
 
 DATABASE_URL = DATABASE_URL.replace(
     "postgresql://",
