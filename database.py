7cee8662cbb968f905fbccadfc3d15fd4527c7b8
trulsasb <trulsasb@gmail.com>
2026-01-15 10:06:40 +0100

    Update database.py
    Ny revidert fil

--- database.py
+++ database.py
@@ -1,11 +1,21 @@
+import os
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from utils.env import settings
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-# Kobling til PostgreSQL
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+DATABASE_URL = os.environ["DATABASE_URL"]
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+psycopg://",
+        1,
+    )
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+)
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
+
