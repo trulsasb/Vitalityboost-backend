ef36295bfb463555ba1cae6275216056dbdf36a3
trulsasb <trulsasb@gmail.com>
2026-01-26 14:49:12 +0100

    Update database.py
    ny databas URL

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL_IN_USE")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
