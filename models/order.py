df801b99d70fba3a2caefab303825ccfd339a7a8
trulsasb <trulsasb@gmail.com>
2026-01-28 11:56:06 +0100

    Update order.py
    databasetilpasning

--- models/order.py
+++ models/order.py
@@ -18,9 +18,7 @@ class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True)
-    # session_id fjernet for å matche eksisterende database
     total_amount = Column(Integer)
-    currency = Column(String, default="NOK")
     status = Column(Enum(OrderStatus), default=OrderStatus.PENDING_PAYMENT)
     created_at = Column(DateTime, default=datetime.utcnow)
 
