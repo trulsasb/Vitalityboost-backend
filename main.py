b5386a65bd66ba4752d66da428e2fd520d41ced4
trulsasb <trulsasb@gmail.com>
2026-01-30 21:51:28 +0100

    Update main.py
    user add

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from database import engine, Base
-from routers import products, cart, orders
+from routers import products, cart, orders, user
 
 # Opprett tabeller hvis de ikke finnes
 Base.metadata.create_all(bind=engine)
@@ -25,6 +25,7 @@ app.add_middleware(
 app.include_router(products.router)
 app.include_router(cart.router)
 app.include_router(orders.router)
+app.include_router(user.router)
 
 @app.get("/")
 def root():
