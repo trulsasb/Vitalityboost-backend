b8eb7ed38fbea33f44eb006efe392ded21a47a14
trulsasb <trulsasb@gmail.com>
2026-02-03 22:08:37 +0100

    Update __init__.py
    clean‑imports‑v1

--- payments/__init__.py
+++ payments/__init__.py
@@ -1,3 +1,10 @@
 from .stripe_handler import StripeHandler
 from .vipps_handler import VippsHandler
 from .router import router
+
+__all__ = [
+    "StripeHandler",
+    "VippsHandler",
+    "router",
+]
+
